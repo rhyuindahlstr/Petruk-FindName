27d2a5ff87d2ac3288fcbe3545ca9ab577a5c127
rhyuindhlstr <149451492+rhyuindhlstr@users.noreply.github.com>
2024-04-27 21:18:33 +0700

    Update PetrukKelompok6.cpp

--- PetrukKelompok6.cpp
+++ PetrukKelompok6.cpp
@@ -65,15 +65,24 @@ int main() {
   )
 
   vector<string> matrix = {
-       "aaflkhpfssuficiclesgnnh","sfvreomrwlrttsxqqnaoao","qeiaifxaeirfvfysximinji",
-		   "wstrlgocapbiafiwiwtuacm","feyaeapistpcrlujkoakcers","rvdakpndeehdemsnckkfoah",
-		   "mrnedslcrriwnrsaafitmmi","yaaecieahymotavhrsstisb","rjsewelccennietohwsglse",
-		   "atanyymoieesnesioireltr","utenewebhmybetnnraieben","rclkuteaeqjlsgshtgdskoa",
-		   "bhoicatnrrsddecehoolgit","ensluarirsetalocohctohe","fzfudqjymadoyiwyglovesu",
-		   "tekalfwonsnaebmiejtzntg","eswposjxeutuyozuwakezhm","kzuhbpezeerflmsnowballh",
-		   "nsnowboardytvwyclevohsa","acocrolgziychodrazzilbi","lbvkkwanzaaqinwolpwonsl",
-		   "bfreezingrainslilgtmelt","hqpylwhfmnffufpswxnummv",
+        "aaflkhpfssuficiclesgnnh","sfvreomrwlrttsxqqnaoao","qeiaifxaeirfvfysximinji",
+        "wstrlgocapbiafiwiwtuacm","feyaeapistpcrlujkoakcers","rvdakpndeehdemsnckkfoah",
+        "mrnedslcrriwnrsaafitmmi","yaaecieahymotavhrsstisb","rjsewelccennietohwsglse",
+	"atanyymoieesnesioireltr","utenewebhmybetnnraieben","rclkuteaeqjlsgshtgdskoa",
+	"bhoicatnrrsddecehoolgit","ensluarirsetalocohctohe","fzfudqjymadoyiwyglovesu",
+	"tekalfwonsnaebmiejtzntg","eswposjxeutuyozuwakezhm","kzuhbpezeerflmsnowballh",
+	"nsnowboardytvwyclevohsa","acocrolgziychodrazzilbi","lbvkkwanzaaqinwolpwonsl",
+	"bfreezingrainslilgtmelt","hqpylwhfmnffufpswxnummv",
   };
 
   for (int i = 0; i < N; i++) {
+      if (searchWord(matrix, words[i])) {
+          cout << "Ada" << endl;
+      } else {
+          cout << "Tidak Ada" << endl;
+      }
+ }
+
+  return 0;
+}
 
