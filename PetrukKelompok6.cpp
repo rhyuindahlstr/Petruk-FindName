95c62891ea0fdeddac5d0f50ba1c0960f2719fbb
alvn26 <151342951+Muhammad26Alvin@users.noreply.github.com>
2024-04-27 20:32:18 +0700

    Create PetrukKelompok6.cpp

---
+++ PetrukKelompok6.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+bool searchword(vector<string> matrix, string word){
+  int rows = matrix.size();
+  int cols = matrix[0].size();
