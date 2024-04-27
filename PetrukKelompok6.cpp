fa97587848c54c5cf6e6a3d7d9a93492e310ac61
rhyuindhlstr <149451492+rhyuindhlstr@users.noreply.github.com>
2024-04-27 20:46:44 +0700

    Update PetrukKelompok6.cpp

--- PetrukKelompok6.cpp
+++ PetrukKelompok6.cpp
@@ -7,3 +7,73 @@ using namespace std;
 bool searchword(vector<string> matrix, string word){
   int rows = matrix.size();
   int cols = matrix[0].size();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main() {
+  int N;
+  cin >> N;
+
+  vector<string> words(N);
+  for (int i = 0; i < N; i++) {
+      cin >> words[i];
+  )
+
+  vector<string> matrix = {
+       "aaflkhpfssuficiclesgnnh","sfvreomrwlrttsxqqnaoao","qeiaifxaeirfvfysximinji",
+		   "wstrlgocapbiafiwiwtuacm","feyaeapistpcrlujkoakcers","rvdakpndeehdemsnckkfoah",
+		   "mrnedslcrriwnrsaafitmmi","yaaecieahymotavhrsstisb","rjsewelccennietohwsglse",
+		   "atanyymoieesnesioireltr","utenewebhmybetnnraieben","rclkuteaeqjlsgshtgdskoa",
+		   "bhoicatnrrsddecehoolgit","ensluarirsetalocohctohe","fzfudqjymadoyiwyglovesu",
+		   "tekalfwonsnaebmiejtzntg","eswposjxeutuyozuwakezhm","kzuhbpezeerflmsnowballh",
+		   "nsnowboardytvwyclevohsa","acocrolgziychodrazzilbi","lbvkkwanzaaqinwolpwonsl",
+		   "bfreezingrainslilgtmelt","hqpylwhfmnffufpswxnummv",
+  };
+
+  for (int i = 0; i < N; i++) {
+
