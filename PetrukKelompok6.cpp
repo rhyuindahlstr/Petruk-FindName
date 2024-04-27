82bd3331af0d4fff5c2720b4e9b03ca2f00252c4
alvn26 <151342951+Muhammad26Alvin@users.noreply.github.com>
2024-04-27 20:50:59 +0700

    Update PetrukKelompok6.cpp

--- PetrukKelompok6.cpp
+++ PetrukKelompok6.cpp
@@ -8,3 +8,30 @@ bool searchword(vector<string> matrix, string word){
   int rows = matrix.size();
   int cols = matrix[0].size();
 
+//Mencari secara diagonal (kiri ke kanan, atas ke bawah)
+for (int i = 0; i < rows; i++){
+  for (int j = 0; j < cols; j++){
+    string diagonal = "";
+    for (int k = 0; i + k < rows && j + k < cols; k++){
+      diagonal += matrix[i + k][j + k];
+      if (diagonal == word){
+        return true;
+      }
+    }
+  }
+}
+
+//Mencari secara diagonal (kiri ke kanan, bawah ke atas)
+for (int i = rows - 1; i >= 0; i--){
+  for (int j = 0; j < cols; j++){
+    string diagonal = "";
+    for (int k = 0; i - k >= 0 && j + k < cols; k++){
+      diagonal += matrix[i - k][j + k];
+      if (diagonal == word){
+        return true;
+      }
+    }
+  }
+}
+return false;
+}
